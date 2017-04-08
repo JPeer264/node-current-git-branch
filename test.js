769c1c9caee7385e340165af18ce66963f665b1d
JPeer264 <jan.oster94@gmail.com>
2017-04-08 12:23:00 +0200

    :white_check_mark: Test: update tests for CIs

--- test.js
+++ test.js
@@ -8,9 +8,24 @@ import branchName from './index';
 
 const fixtures = 'test/fixtures';
 
-test('check if the given directory is the right branch', (t) => {
-  t.is(branchName(path.join(fixtures, 'master')), 'master');
-  t.is(branchName(path.join(fixtures, 'feat_test')), 'feat/test');
+test('check if the given directory is the branch master', (t) => {
+  const headPath = path.join(fixtures, 'master', '.git', 'HEAD');
+
+  fs.writeFileSync(headPath, 'ref: refs/heads/master');
+
+  const headFile = fs.readFileSync(headPath, 'utf8').replace(/\n/, '');
+
+  t.is(branchName(path.join(fixtures, 'master')), headFile.slice(16, headFile.length));
+});
+
+test('check if the given directory is the branch feat/test', (t) => {
+  const headPath = path.join(fixtures, 'master', '.git', 'HEAD');
+
+  fs.writeFileSync(headPath, 'ref: refs/heads/feat/test');
+
+  const headFile = fs.readFileSync(path.join(fixtures, 'feat_test', '.git', 'HEAD'), 'utf8').replace(/\n/, '');
+
+  t.is(branchName(path.join(fixtures, 'feat_test')), headFile.slice(16, headFile.length));
 });
 
 test('check the branch name of the home dir', (t) => {
