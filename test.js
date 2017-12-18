b2a4d80487ad7b7c99510c670036eb28d6ec7c5d
JPeer264 <jan.oster94@gmail.com>
2017-12-18 21:01:29 +0100

    Test: fix tests

--- test.js
+++ test.js
@@ -1,30 +1,31 @@
+import fs from 'fs';
 import { homedir } from 'os';
 import test from 'ava';
 import path from 'path';
-import isCi from 'is-ci';
 
 import branchName from './index';
 
 const fixtures = 'test/fixtures';
 
-test('check if the given directory is the branch master', (t) => {
-  t.plan(1);
+const folders = [
+  'feat_test',
+  'master',
+];
 
-  if (isCi) {
-    t.is(branchName(path.join(fixtures, 'master')).slice(0, 14), '(HEAD detached');
-  } else {
-    t.is(branchName(path.join(fixtures, 'master')), 'master');
-  }
+test.before('rename git folders', () => {
+  folders.map(folder => fs.renameSync(path.join(fixtures, folder, 'git'), path.join(fixtures, folder, '.git')));
+});
+
+test.after.always('rename .git folders', () => {
+  folders.map(folder => fs.renameSync(path.join(fixtures, folder, '.git'), path.join(fixtures, folder, 'git')));
+});
+
+test('check if the given directory is the branch master', (t) => {
+  t.is(branchName(path.join(fixtures, 'master')), 'master');
 });
 
 test('check if the given directory is the branch feat/test', (t) => {
-  t.plan(1);
-
-  if (isCi) {
-    t.is(branchName(path.join(fixtures, 'feat_test')).slice(0, 14), '(HEAD detached');
-  } else {
-    t.is(branchName(path.join(fixtures, 'feat_test')), 'feat/test');
-  }
+  t.is(branchName(path.join(fixtures, 'feat_test')), 'feat_test');
 });
 
 test('check the branch name of the home dir', (t) => {
